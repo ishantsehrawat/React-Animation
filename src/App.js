8127b9550c5528eb914bb1e441eb937e14707119
ishantsehrawat <ishantsehrawat75@gmail.com>
2022-02-14 20:58:57 +0530

    ep2- three circles

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import logo from './logo.svg';
-import { useRef, useEffect } from 'react';
-import {gsap, Power3} from 'gsap';
+import { useEffect, useState } from 'react';
+import { gsap, Power3 } from 'gsap';
+import { useRef } from 'react';
 import './App.css';
+import { TweenMax } from 'gsap/gsap-core';
+
 
 function App() {
-  let logoitem  = useRef(null);
-  let textitem = useRef(null);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [state, setState] = useState(false);
+  
+  const handleExpand = () => {
+    gsap.to(circleRed, .8, {width: 200, height: 200, ease: Power3.easeOut})
+    setState(true)
+  }
+  const handleShrink = () => {
+    gsap.to(circleRed, .8, {width: 75, height: 75, ease: Power3.easeOut})
+    setState(false)
+  }
 
   useEffect(() => {
-    console.log(logoitem);
-    gsap.to(
-      ".App-logo",
-      .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut
-      }
-    )
-    gsap.to(
-      ".text",
-      .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut,
-        delay: 0.2
-      }
-    )
+    gsap.to(".App", 0, {css: {visibility: 'visible'}})
+    // gsap.from(circle, .8, {opacity: 0, x: 80, ease: Power3.easeOut}) 
+    // gsap.from(circleRed, .8, {opacity: 0, x: 80, ease: Power3.easeOut, delay: .2})    
+    // gsap.from(circleBlue, .8, {opacity: 0, x: 80, ease: Power3.easeOut, delay: .4})    
+    TweenMax.staggerFrom([circle, circleRed, circleBlue], .8, {opacity: 0, x: 80, ease: Power3.easeOut}, .2)
   }, [])
+
   return (
-    <div className="App">
+    <div className="App" ref={el => {app = el}}>
       <header className="App-header">
-        <img src={logo} 
-        ref={el => {logoitem = el}}
-        className="App-logo" alt="logo" />
-        <p ref={el => {textitem = el}} className="text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='circle-container'>
+          <div 
+          ref={el => {circle = el}}
+          className='circle'></div>
+          <div 
+          ref={el => {circleRed = el}}
+          onClick={state !== true ? handleExpand : handleShrink}
+          className='circle red'></div>
+          <div 
+          ref={el => {circleBlue = el}}
+          className='circle blue'></div>
+        </div>
       </header>
     </div>
   );
